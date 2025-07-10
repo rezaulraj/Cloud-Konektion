de44f22dc5e0d51e7d98ec6b9b20a10fbe5adb25
rezaul-karim <rezaul.coderpro@gmail.com>
2025-07-10 10:48:20 +0600

    update service route commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="about-us" element={<AboutPage />} />
           <Route path="for-candidates" element={<CandidatePage />} />
           <Route path="join-us" element={<JoinUsPage />} />
-          <Route path="serivces" element={<ServicePage />} />
+          <Route path="services" element={<ServicePage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
